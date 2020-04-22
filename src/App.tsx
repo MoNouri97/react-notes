ddfc66768a12ec550cf79b8e966fbfa755d6aa32
Nouri Mohamed <mohamed.nouri.1997@gmail.com>
2020-04-22 01:08:28 +0100

    started on routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 import './App.scss';
 import Navbar from './components/Navbar';
 import List from './components/List';
@@ -6,13 +8,25 @@ import NotesContextProvider from './contexts/NotesContext';
 import AddNote from './components/AddNote';
 
 function App() {
+	const onAddNote = () => {
+		// TODO
+	};
+
 	return (
 		<div className='App'>
 			<Navbar />
 			<div className='page-content'>
 				<NotesContextProvider>
-					<AddNote />
-					<List />
+					<Router>
+						<Switch>
+							<Route path='/add'>
+								<AddNote submit={onAddNote} />
+							</Route>
+							<Route path='/'>
+								<List />
+							</Route>
+						</Switch>
+					</Router>
 				</NotesContextProvider>
 			</div>
 		</div>
--- src/components/AddNote.tsx
+++ src/components/AddNote.tsx
@@ -2,45 +2,71 @@ import React, { useState, FormEvent, ChangeEvent } from 'react';
 import PropTypes from 'prop-types';
 import Note from '../models/note.model';
 
+AddNote.propTypes = {
+	submit: PropTypes.func.isRequired,
+	note: PropTypes.instanceOf(Note),
+};
+
 function AddNote(props: PropTypes.InferProps<typeof AddNote.propTypes>) {
 	const [body, setBody] = useState('');
 	const [title, setTitle] = useState('');
 
 	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		console.log({ title });
+		console.log({ title, body });
+		props.submit(title, body);
 	};
 
-	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+	const handleChangeTitle = (e: ChangeEvent<HTMLInputElement>) => {
 		setTitle(e.target.value);
+	};
+	const handleChangeBody = (e: ChangeEvent<HTMLTextAreaElement>) => {
+		setBody(e.target.value);
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<div className='field'>
-				<p className='control has-icons-left'>
-					<input
-						className='input'
-						type='title'
-						value={title}
-						onChange={handleChange}
-						placeholder='Filter'
-					/>
-					<span className='icon is-small is-left'>
-						<i data-feather='search'></i>
-					</span>
-				</p>
-			</div>
-			{/* <input type='title' value={title} onChange={handleChange} /> */}
-			<button className='button is-primary' type='submit'>
-				Add
-			</button>
-		</form>
+		<div className='form-container'>
+			<form onSubmit={handleSubmit}>
+				<div className='field'>
+					<label className='label'>Title</label>
+					<div className='control'>
+						<input
+							className='input'
+							type='text'
+							value={title}
+							onChange={handleChangeTitle}
+							placeholder='finish the assignment'
+						/>
+					</div>
+				</div>
+				<div className='field'>
+					<label className='label'>Body</label>
+					<div className='control'>
+						<textarea
+							className='textarea'
+							value={body}
+							onChange={handleChangeBody}
+							placeholder='body . . .'
+							rows={10}
+						></textarea>
+					</div>
+				</div>
+				{/* <input type='title' value={title} onChange={handleChange} /> */}
+				<div className='field is-grouped is-pulled-right'>
+					<div className='control'>
+						<button type='button' className='button is-text'>
+							Cancel
+						</button>
+					</div>
+					<div className='control'>
+						<button type='submit' className='button is-success'>
+							Save
+						</button>
+					</div>
+				</div>
+			</form>
+		</div>
 	);
 }
-
-AddNote.propTypes = {
-	// note: PropTypes.instanceOf(Note),
-};
 
 export default AddNote;

1f52d5a0989c20cb9edab37a172efd93ed485ab5
Nouri Mohamed <mohamed.nouri.1997@gmail.com>
2020-04-24 23:13:06 +0100

    Added Filter

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,11 +1,80 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { NotesContext } from '../contexts/NotesContext';
 import NoteCard from './NoteCard';
 import { Link } from 'react-router-dom';
+import Note from '../models/note.model';
 
 export default function List() {
-	const { notes } = useContext(NotesContext);
+	const { notes, deleteNote } = useContext(NotesContext);
+	const [filtered, setFiltered] = useState(notes);
 
+	//#region Filter
+	const filter = (query: string) => {
+		// getting words
+		query = query.toLowerCase().trim();
+
+		if (query == '') {
+			// empty search
+			setFiltered(notes);
+			return;
+		}
+
+		let terms: string[] = query.split(' ');
+
+		// removing duplicates
+		terms = removeDuplicates(terms);
+
+		let allResults: Note[] = new Array<Note>();
+
+		terms.forEach((term) => {
+			let results: Note[] = relevantNotes(term);
+			allResults.push(...results);
+		});
+
+		setFiltered(sortByRelevancy(allResults));
+	};
+
+	const removeDuplicates = (arr: Array<any>): Array<any> => {
+		let unique: Set<any> = new Set<any>();
+
+		arr.forEach((e) => unique.add(e));
+		return Array.from(unique);
+	};
+
+	const relevantNotes = (query: string): Note[] => {
+		query = query.toLowerCase().trim();
+		let relevant = notes!.filter((note) => {
+			return (
+				(note.body && note.body.toLowerCase().trim().includes(query)) ||
+				note.title.toLowerCase().trim().includes(query)
+			);
+		});
+		return relevant;
+	};
+
+	const sortByRelevancy = (searchRes: Note[]) => {
+		let uniqueResults = removeDuplicates(searchRes);
+		let noteCount: { [x: string]: number } = {};
+
+		searchRes.forEach((note) => {
+			let noteId = note.id;
+
+			if (noteCount && noteCount[noteId]) {
+				noteCount[noteId] += 1;
+			} else {
+				noteCount[noteId] = 1;
+			}
+		});
+		console.log({ noteCount });
+
+		const sorted = uniqueResults.sort((a, b) => {
+			return noteCount[b.id] - noteCount[a.id];
+		});
+		return sorted;
+	};
+	//#endregion
+
+	//#region render
 	return (
 		<div className='mainLayout'>
 			<div className='main-container'>
@@ -17,6 +86,9 @@ export default function List() {
 								type='text'
 								placeholder='Filter'
 								//   (keyup)="filter($event.target.value)"
+								onChange={(e) => {
+									filter(e.target.value);
+								}}
 							/>
 							<span className='icon is-small is-left'>
 								<i data-feather='search'></i>
@@ -25,7 +97,13 @@ export default function List() {
 					</div>
 				</div>
 				<div className='notes-list'>
-					{notes && notes.map((n) => <NoteCard key={n.id} note={n} />)}
+					{filtered && filtered.length > 0 ? (
+						filtered.map((n) => (
+							<NoteCard key={n.id} note={n} delete={deleteNote!} />
+						))
+					) : (
+						<p>No More Notes . . .</p>
+					)}
 				</div>
 				<Link className='button floating-add-button is-primary' to='/add'>
 					+ Add
@@ -33,4 +111,5 @@ export default function List() {
 			</div>
 		</div>
 	);
+	//#endregion
 }
